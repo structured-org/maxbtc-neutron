5a2aaf10f52b646975c3eed92637229178b702a1
Sergey Ratiashvili <serg.s.r@gmail.com>
2025-10-08 12:28:30 +0200

    feat: zk-me

--- contracts/maxbtc-neutron-allow-list/src/state.rs
+++ contracts/maxbtc-neutron-allow-list/src/state.rs
@@ -1,4 +1,7 @@
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
+use crate::msg::ZkMeSettings;
+
 pub const ALLOW_LIST: Item<Vec<Addr>> = Item::new("allow_list");
+pub const ZK_ME_SETTINGS: Item<ZkMeSettings> = Item::new("zk_me_settings");
