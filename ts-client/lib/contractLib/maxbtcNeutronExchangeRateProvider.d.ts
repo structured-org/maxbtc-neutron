78214361ae88765d012832298422b76989499846
Albert Andrejev <albert.andrejev@gmail.com>
2025-11-27 15:23:27 +0200

    fixes after rebase

--- ts-client/lib/contractLib/maxbtcNeutronExchangeRateProvider.d.ts
+++ ts-client/lib/contractLib/maxbtcNeutronExchangeRateProvider.d.ts
@@ -1,6 +1,11 @@
-import { CosmWasmClient, SigningCosmWasmClient, ExecuteResult, InstantiateResult } from "@cosmjs/cosmwasm-stargate";
-import { StdFee } from "@cosmjs/amino";
-import { Coin } from "@cosmjs/amino";
+import {
+  CosmWasmClient,
+  SigningCosmWasmClient,
+  ExecuteResult,
+  InstantiateResult,
+} from '@cosmjs/cosmwasm-stargate';
+import { StdFee } from '@cosmjs/amino';
+import { Coin } from '@cosmjs/amino';
 /**
  * An implementation of i256 that is using strings for JSON encoding/decoding, such that the full i256 range can be used for clients that convert JSON numbers to floats, like JavaScript and jq.
  *
@@ -18,15 +23,24 @@ export type Int256 = string;
  */
 export type Decimal = string;
 /**
+ * A fixed-point decimal value with 18 fractional digits, i.e. Decimal(1_000_000_000_000_000_000) == 1.0
+ *
+ * The greatest possible value that can be represented is 340282366920938463463.374607431768211455 (which is (2^128 - 1) / 10^18)
+ */
+export type Decimal1 = string;
+/**
  * Expiration represents a point in time when some event happens. It can compare with a BlockInfo and will return is_expired() == true once the condition is hit (and for every block in the future)
  */
-export type Expiration = {
-    at_height: number;
-} | {
-    at_time: Timestamp;
-} | {
-    never: {};
-};
+export type Expiration =
+  | {
+      at_height: number;
+    }
+  | {
+      at_time: Timestamp;
+    }
+  | {
+      never: {};
+    };
 /**
  * A point in time in nanosecond precision.
  *
@@ -54,82 +68,125 @@ export type Uint64 = string;
 /**
  * Actions that can be taken to alter the contract's ownership
  */
-export type UpdateOwnershipArgs = {
-    transfer_ownership: {
+export type UpdateOwnershipArgs =
+  | {
+      transfer_ownership: {
         expiry?: Expiration | null;
         new_owner: string;
-    };
-} | "accept_ownership" | "renounce_ownership";
+      };
+    }
+  | 'accept_ownership'
+  | 'renounce_ownership';
 export interface MaxbtcNeutronExchangeRateProviderSchema {
-    responses: GetAumResponse | GetTwaerResponse | OwnershipForString;
-    execute: UpdateExchangeRateArgs | UpdateAumArgs | UpdateOwnershipArgs;
-    instantiate?: InstantiateMsg;
-    [k: string]: unknown;
+  responses: Decimal | GetTwaerResponse | OwnershipForString;
+  execute: UpdateExchangeRateArgs | UpdateOwnershipArgs;
+  instantiate?: InstantiateMsg;
+  [k: string]: unknown;
 }
 /**
  * Response type for AUM Oracle receiver GetAum queries. **Must** return the AUM in micro-Bitcoin (uwBTC) with precision of `WBTC_DECIMALS`.
  */
 export interface GetAumResponse {
-    /**
-     * The latest AUM in the AUM Oracle receiver reported by messengers scaled to `WBTC_DECIMALS`.
-     */
-    aum_in_wbtc: Int256;
-    /**
-     * Represents the number of decimals that the `aum_in_wbtc` is represented in. Must always equal to `WBTC_DECIMALS`. It is used to scale the `aum_in_wbtc` to its base BTC value. E.g. `base_aum_in_btc = aum_in_wbtc / 10^decimals`.
-     */
-    decimals: number;
+  /**
+   * The latest AUM in the AUM Oracle receiver reported by messengers scaled to `WBTC_DECIMALS`.
+   */
+  aum_in_wbtc: Int256;
+  /**
+   * Represents the number of decimals that the `aum_in_wbtc` is represented in. Must always equal to `WBTC_DECIMALS`. It is used to scale the `aum_in_wbtc` to its base BTC value. E.g. `base_aum_in_btc = aum_in_wbtc / 10^decimals`.
+   */
+  decimals: number;
 }
 export interface GetTwaerResponse {
-    published_at: number;
-    twaer: Decimal;
+  published_at: number;
+  twaer: Decimal1;
 }
 /**
  * The contract's ownership info
  */
 export interface OwnershipForString {
-    /**
-     * The contract's current owner. `None` if the ownership has been renounced.
-     */
-    owner?: string | null;
-    /**
-     * The deadline for the pending owner to accept the ownership. `None` if there isn't a pending ownership transfer, or if a transfer exists and it doesn't have a deadline.
-     */
-    pending_expiry?: Expiration | null;
-    /**
-     * The account who has been proposed to take over the ownership. `None` if there isn't a pending ownership transfer.
-     */
-    pending_owner?: string | null;
+  /**
+   * The contract's current owner. `None` if the ownership has been renounced.
+   */
+  owner?: string | null;
+  /**
+   * The deadline for the pending owner to accept the ownership. `None` if there isn't a pending ownership transfer, or if a transfer exists and it doesn't have a deadline.
+   */
+  pending_expiry?: Expiration | null;
+  /**
+   * The account who has been proposed to take over the ownership. `None` if there isn't a pending ownership transfer.
+   */
+  pending_owner?: string | null;
 }
 export interface UpdateExchangeRateArgs {
-    rate: Decimal;
+  rate: Decimal1;
 }
 export interface UpdateAumArgs {
-    aum_in_wbtc: Int256;
-    decimals: number;
+  aum_in_wbtc: Int256;
+  decimals: number;
 }
 export interface InstantiateMsg {
-    owner: string;
+  owner: string;
 }
 export declare class Client {
-    private readonly client;
-    contractAddress: string;
-    constructor(client: CosmWasmClient | SigningCosmWasmClient, contractAddress: string);
-    mustBeSigningClient(): Error;
-    static instantiate(client: SigningCosmWasmClient, sender: string, codeId: number, initMsg: InstantiateMsg, label: string, fees: StdFee | 'auto' | number, initCoins?: readonly Coin[], admin?: string): Promise<InstantiateResult>;
-    static instantiate2(client: SigningCosmWasmClient, sender: string, codeId: number, salt: Uint8Array, initMsg: InstantiateMsg, label: string, fees: StdFee | 'auto' | number, initCoins?: readonly Coin[], admin?: string): Promise<InstantiateResult>;
-    queryGetTwaer: () => Promise<GetTwaerResponse>;
-    queryGetAum: () => Promise<GetAumResponse>;
-    queryOwnership: () => Promise<OwnershipForString>;
-    updateExchangeRate: (sender: string, args: UpdateExchangeRateArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
-    updateExchangeRateMsg: (args: UpdateExchangeRateArgs) => {
-        update_exchange_rate: UpdateExchangeRateArgs;
-    };
-    updateAum: (sender: string, args: UpdateAumArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
-    updateAumMsg: (args: UpdateAumArgs) => {
-        update_aum: UpdateAumArgs;
-    };
-    updateOwnership: (sender: string, args: UpdateOwnershipArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
-    updateOwnershipMsg: (args: UpdateOwnershipArgs) => {
-        update_ownership: UpdateOwnershipArgs;
-    };
+  private readonly client;
+  contractAddress: string;
+  constructor(
+    client: CosmWasmClient | SigningCosmWasmClient,
+    contractAddress: string,
+  );
+  mustBeSigningClient(): Error;
+  static instantiate(
+    client: SigningCosmWasmClient,
+    sender: string,
+    codeId: number,
+    initMsg: InstantiateMsg,
+    label: string,
+    fees: StdFee | 'auto' | number,
+    initCoins?: readonly Coin[],
+    admin?: string,
+  ): Promise<InstantiateResult>;
+  static instantiate2(
+    client: SigningCosmWasmClient,
+    sender: string,
+    codeId: number,
+    salt: Uint8Array,
+    initMsg: InstantiateMsg,
+    label: string,
+    fees: StdFee | 'auto' | number,
+    initCoins?: readonly Coin[],
+    admin?: string,
+  ): Promise<InstantiateResult>;
+  queryGetTwaer: () => Promise<GetTwaerResponse>;
+  queryExchangeRate: () => Promise<Decimal>;
+  queryOwnership: () => Promise<OwnershipForString>;
+  updateExchangeRate: (
+    sender: string,
+    args: UpdateExchangeRateArgs,
+    fee?: number | StdFee | 'auto',
+    memo?: string,
+    funds?: Coin[],
+  ) => Promise<ExecuteResult>;
+  updateExchangeRateMsg: (args: UpdateExchangeRateArgs) => {
+    update_exchange_rate: UpdateExchangeRateArgs;
+  };
+  updateAum: (
+    sender: string,
+    args: UpdateAumArgs,
+    fee?: number | StdFee | 'auto',
+    memo?: string,
+    funds?: Coin[],
+  ) => Promise<ExecuteResult>;
+  updateAumMsg: (args: UpdateAumArgs) => {
+    update_aum: UpdateAumArgs;
+  };
+  updateOwnership: (
+    sender: string,
+    args: UpdateOwnershipArgs,
+    fee?: number | StdFee | 'auto',
+    memo?: string,
+    funds?: Coin[],
+  ) => Promise<ExecuteResult>;
+  updateOwnershipMsg: (args: UpdateOwnershipArgs) => {
+    update_ownership: UpdateOwnershipArgs;
+  };
 }
