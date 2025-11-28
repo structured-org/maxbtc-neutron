87771059912b265a00adf47069fc209b233e58a0
Sergey Ratiashvili <serg.s.r@gmail.com>
2025-11-28 17:02:53 +0100

    refactor: update allow list to use unit type and improve attribute handling in execute and test functions

--- contracts/maxbtc-neutron-allow-list/src/state.rs
+++ contracts/maxbtc-neutron-allow-list/src/state.rs
@@ -4,5 +4,5 @@ use cw_storage_plus::{Item, Map};
 use crate::msg::ZkMeSettings;
 
 pub const ALLOW_LIST_V1: Item<Vec<Addr>> = Item::new("allow_list");
-pub const ALLOW_LIST: Map<&Addr, bool> = Map::new("allow_list_v2");
+pub const ALLOW_LIST: Map<&Addr, ()> = Map::new("allow_list_v2");
 pub const ZK_ME_SETTINGS: Item<ZkMeSettings> = Item::new("zk_me_settings");
