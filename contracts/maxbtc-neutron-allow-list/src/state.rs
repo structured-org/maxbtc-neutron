2a69c1da6a1935a01cea83ea93dcc7f840c9e52c
Sergey R <serg.s.r@gmail.com>
2025-12-02 14:05:15 +0300

    fix: audit report 33 kv allowlist (#55)
    * refactor: update allow list functionality and version to 0.3.0
    * refactor: update allow list to use unit type and improve attribute handling in execute and test functions
    * chore: update cargo.lock

--- contracts/maxbtc-neutron-allow-list/src/state.rs
+++ contracts/maxbtc-neutron-allow-list/src/state.rs
@@ -1,7 +1,8 @@
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 use crate::msg::ZkMeSettings;
 
-pub const ALLOW_LIST: Item<Vec<Addr>> = Item::new("allow_list");
+pub const ALLOW_LIST_V1: Item<Vec<Addr>> = Item::new("allow_list");
+pub const ALLOW_LIST: Map<&Addr, ()> = Map::new("allow_list_v2");
 pub const ZK_ME_SETTINGS: Item<ZkMeSettings> = Item::new("zk_me_settings");
